811149209e77cb1c0defb9dbe68eda40bd0d3dfb
gt0203 <gt_0203@hotmail.com>
2018-12-17 12:15:34 +0100

    first upload v1.0.1

---
+++ README.md
@@ -0,0 +1,21 @@
+# Chronomètre
+
+Chronomètre 2018, Thomas Gerard
+
+# Screenshot
+
+![alt text](http://image.noelshack.com/fichiers/2018/51/1/1545045028-capture-du-2018-12-17-12-10-03.png)
+
+## Description
+
+Application de chronomètre codé en HTML,CSS et JS par mes soins
+
+## technologie utilisé
+
+Boostrap vanilla
+
+## Usage
+
+Upload all on your server, after open your mydomaine.fr/cv/index.html
+
+
---
+++ js/script.js
@@ -0,0 +1,78 @@
+window.onload = function () {
+  // On déclare les variables
+    var secondes = 00; 
+    var number = 00; 
+    var minutes = 00;
+    var appendnumber = document.getElementById("number")
+    var appendSecondes = document.getElementById("secondes")
+    var appendMinutes = document.getElementById("minutes")
+    var boutonStart = document.getElementById('bouton-start');
+    var boutonStop = document.getElementById('bouton-stop');
+    var boutonReset = document.getElementById('bouton-reset');
+    var Interval ;
+  
+    boutonStart.onclick = function() {
+      
+       clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
+    }
+    
+      boutonStop.onclick = function() {
+         clearInterval(Interval);
+    }
+    
+  
+    boutonReset.onclick = function() {
+       clearInterval(Interval);
+        minutes = "00"; 
+        number = "00";
+        secondes = "00";
+        appendMinutes.innerHTML = minutes;
+        appendnumber.innerHTML = number;
+        appendSecondes.innerHTML = secondes;
+    }
+    
+     
+    
+    function startTimer () {
+      number++; 
+      // si le nombre est en dessous de 9, on met le "0" en premiere position
+      if(number < 9){
+        appendnumber.innerHTML = "0" + number;
+      }
+      // si le nombre est au dessus de 9, on enleve le "0" en premiere position
+      if (number > 9){
+        appendnumber.innerHTML = number;
+        
+      } 
+      
+      if (number > 99) {
+        console.log("secondes");
+        secondes++;    
+        appendSecondes.innerHTML = "0" + secondes;
+        number = 0;
+        appendnumber.innerHTML = "0" + 0;
+      }
+      
+      if (secondes > 9){
+        appendSecondes.innerHTML = secondes;
+      }
+
+      if (secondes > 59) {
+        console.log("minutes");
+        minutes++;
+        appendMinutes.innerHTML = "0" + minutes;
+        secondes = 0;
+        appendSecondes.innerHTML = "0" + 0;
+      }
+      
+      if (minutes > 9){
+        appendMinutes.innerHTML = minutes;
+      }
+    
+    }
+    
+  
+  }
+
+  
